ef81f6e47e6aba7f2d490c925a8964040057b7aa
tattoo1880 <tattoo186225345941@gmail.com>
2024-12-20 23:46:46 +0800

    so

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ async def getdata(dict1):
     filename = dict1["filename"]
     url = dict1["url"]
 
-    cookies = {
-        'sajssdk_2015_cross_new_user': '1',
-        'sensorsdata2015jssdkcross': '%7B%22distinct_id%22%3A%22193d23afa3475-0ef24df5770b968-1e525636-1296000-193d23afa359ce%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%22193d23afa3475-0ef24df5770b968-1e525636-1296000-193d23afa359ce%22%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkzZDIzYjAxM2NiMzEtMGQzZTUwN2NiYmYxYWMtMWU1MjU2MzYtMTI5NjAwMC0xOTNkMjNiMDEzZGUzNCJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%7D',
-        'Hm_lvt_a19fd7224d30e3c8a6558dcb38c4beed': '1734404193',
-        'HMACCOUNT': '9AB16716FC5BB47C',
-        'acw_tc': '1a0c39d517344041940112848e0143fa67906f4c52c3aa30c08470c45426bb',
-        'Hm_lpvt_a19fd7224d30e3c8a6558dcb38c4beed': '1734404279',
-    }
+    # cookies = {
+    #     'sajssdk_2015_cross_new_user': '1',
+    #     'sensorsdata2015jssdkcross': '%7B%22distinct_id%22%3A%22193d23afa3475-0ef24df5770b968-1e525636-1296000-193d23afa359ce%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%22193d23afa3475-0ef24df5770b968-1e525636-1296000-193d23afa359ce%22%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkzZDIzYjAxM2NiMzEtMGQzZTUwN2NiYmYxYWMtMWU1MjU2MzYtMTI5NjAwMC0xOTNkMjNiMDEzZGUzNCJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%7D',
+    #     'Hm_lvt_a19fd7224d30e3c8a6558dcb38c4beed': '1734404193',
+    #     'HMACCOUNT': '9AB16716FC5BB47C',
+    #     'acw_tc': '1a0c39d517344041940112848e0143fa67906f4c52c3aa30c08470c45426bb',
+    #     'Hm_lpvt_a19fd7224d30e3c8a6558dcb38c4beed': '1734404279',
+    # }
 
     headers = {
         'Accept': '*/*',
@@ -48,7 +48,7 @@ async def getdata(dict1):
     async with httpx.AsyncClient() as client:
         response = await client.post(
             url,
-            cookies=cookies,
+            # cookies=cookies,
             headers=headers,
             json=json_data,
         )
